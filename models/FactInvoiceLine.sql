dd4c4095197e3bf5d62a2a149ceebc708abba79b
Justin Kolpak <justin.kolpak@databricks.com>
2023-10-19 17:28:57 +0000

    DimCity, DimCustomer, FactInvoiceLines  created

--- models/FactInvoiceLine.sql
+++ models/FactInvoiceLine.sql
@@ -1,5 +1,21 @@
+{{ config(materialized='table') }}
 
--- Use the `ref` function to select from other models
 
-select *
-from {{ ref('DimCity') }}
+SELECT il.InvoiceLineId
+    , i.InvoiceID
+    , i.InvoiceDate
+    , o.OrderID
+    , o.OrderDate
+    , o.CustomerID
+    , si.StockItemID
+    , si.StockItemName
+    , il.Quantity
+    , il.UnitPrice
+    , il.TaxRate
+    , il.TaxAmount
+    , il.LineProfit
+    , il.ExtendedPrice
+FROM {{ source('wideworldimporters', 'bronze_salesinvoicelines') }} il
+LEFT JOIN {{ source('wideworldimporters', 'bronze_salesinvoices') }} i ON il.InvoiceID = i.InvoiceID
+LEFT JOIN {{ source('wideworldimporters', 'bronze_salesorders') }} o ON i.OrderID = o.OrderID
+LEFT JOIN {{ source('wideworldimporters', 'bronze_warehousestockitems') }}  si ON il.StockItemID = si.StockItemID
